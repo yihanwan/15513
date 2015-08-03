bf9e9cf4db7bc1a7aa151d7434f6e1524b46d58c
Yihan Wang <yihanwan@andrew.cmu.edu>
2015-08-03 11:17:47 -0400

    hw7

--- hw6/malloclab-handout/mm.c
+++ hw6/malloclab-handout/mm.c
@@ -1,8 +1,15 @@
 /*
  * mm.c
  *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
+ * Yihan Wang
+ * yihanwan
+ * Solution:
+ * Use explicit list for free blocks
+ * Use constant-time coalescing
+ * Free block structure: 
+ * |hdr|pointer to prev free block|pointer to next free block|...|ftr|
+ * Allocated block structure:
+ * |hdr|...|ftr|
  */
 #include <assert.h>
 #include <stdio.h>
@@ -14,12 +21,13 @@
 #include "memlib.h"
 
 
-/* word, 8 bytes */
+/* word */
 #define WSIZE 8
+/* double word */
 #define DSIZE 16
+/* each block is at least 32 bytes 
+ |hdr|pointer|pointer|ftr|*/
 #define FSIZE 32
-/* the thresh to index blocks in BST */
-#define THRESH 4
 /* extend heap by this amount */
 #define CHUNKSIZE (1<<12)
 
@@ -29,18 +37,28 @@
 /* read and write a word at address p */
 #define GET(p) (*(unsigned long long *)(p))
 #define PUT(p, val) (*(unsigned long long *)(p)=((unsigned long long)(val)))
+
+/* get hdr/ftr of a block */
 #define HDRP(p) ((void *)(p) - WSIZE)
 #define FTRP(p) ((void *)(p) + GET_SIZE(p) - DSIZE)
+
+/* get block size */
 #define GET_SIZE(p) ((GET(HDRP(p)))&~0x7)
+
+/* 1:allocated; 0:free */ 
 #define GET_ALLOC(p) ((GET(HDRP(p)))&0x1)
+
+/* get next/prev free block in the list */
 #define GET_NEXT(p) ((void *)(GET(p+WSIZE)))
 #define GET_PREV(p) ((void *)(GET(p)))
+
+/* set block p's prev/next pointer */
 #define SET_NEXT(p, val) (PUT(p+WSIZE, val))
 #define SET_PREV(p, val) (PUT(p, val))
+
+/* get prev/next block in the heap*/
 #define NEXT_BLKP(bp) ((void *)(bp) + GET_SIZE((void *)(bp)))
 #define PREV_BLKP(bp) ((void *)(bp) - GET_SIZE((void *)(bp) - WSIZE))
-
-
 
 /* max of two number */
 #define MAX(x, y) ( (x)>(y)? (x): (y) )
@@ -70,103 +88,118 @@
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(p) (((size_t)(p) + (ALIGNMENT-1)) & ~0x7)
 
-
+/* functions */
 static void *coalesce(void *bp);
 static void *extend_heap(size_t size);
 static void *find_fit(size_t size);
 static void *place(void *bp, size_t size);
 static void coalesce_next(void *bp);
+
+/* prologue block */
 void *heap_listp;
+
+/* starter of the free list */
 void *free_listp;
 
 /*
  * Initialize: return -1 on error, 0 on success.
  */
 int mm_init(void) {
-    /* create the initial empty heap */
-
-    if((heap_listp = mem_sbrk(7 * WSIZE)) == (void *)-1)
-        return -1;
-	PUT(heap_listp, 0);   //list starter.prev
-    PUT(heap_listp + WSIZE * 1, heap_listp+ WSIZE*2);   //list starter.next
-    PUT(heap_listp + 2 * WSIZE, heap_listp);   //list ender.prev
-    PUT(heap_listp + (3 * WSIZE), 0);  //list ender.next
+	/* create the initial empty heap */
+	if((heap_listp = mem_sbrk(7 * WSIZE)) == (void *)-1)
+		return -1;
+	
+	/* init free list */
+	PUT(heap_listp, 0);   //set explicit list starter's prev pointer
+	PUT(heap_listp + WSIZE * 1, heap_listp+ WSIZE*2);   //set list starter's next pointer
+	PUT(heap_listp + 2 * WSIZE, heap_listp);   //list ender's prev pointer
+	PUT(heap_listp + (3 * WSIZE), 0);  //list ender's next pointer
+	
+	/* init prologue/epilogue */
 	PUT(heap_listp + (4 * WSIZE), PACK(DSIZE, 1));  //prologue hdr
 	PUT(heap_listp + (5 * WSIZE), PACK(DSIZE, 1));  //prologue ftr
 	PUT(heap_listp + (6 * WSIZE), PACK(0, 1));  //epilogue hdr
+	
+	/* init pointers */
 	free_listp = heap_listp;
 	heap_listp += 5*WSIZE;	
+	
 	/* extend the empty heap with a free block of CHUCKSIZE bytes */
-    if(extend_heap(CHUNKSIZE / WSIZE) == NULL) return -1;
-    return 0;
+	if(extend_heap(CHUNKSIZE / WSIZE) == NULL) return -1;
+	return 0;
 }
 
 /*
  * malloc
+ * basic idea:
+ * find the suitable free block from the free list
+ * if not found, extend the heap.
  */
 void *malloc (size_t size) {
-    size_t asize; /* adjusted block size */
-    void *bp;
-    size_t extendsize;
-//printf("malloc, %d\n",(int)size);
-    /* ignore spurious requests */
-    if(size <= 0)
-        return NULL;
-if (size<DSIZE){
-size = DSIZE;
-}
-    /* adjust block size to include meta-data in head/tail */
-    asize = ALIGN(size + DSIZE);
-//mm_checkheap(11);    
-/* search the free list for a fit */
+	size_t asize; /* adjusted block size */
+	void *bp;
+	size_t extendsize;
+	
+	/* ignore spurious requests */
+	if(size <= 0)
+		return NULL;
+	
+	/* each block should be at least FSIZE
+	 * so the alloc size should be at least DSIZE */
+	if (size<DSIZE){
+		size = DSIZE;
+	}
+	
+	/* adjust block size to include hdr/ftr */
+	asize = ALIGN(size + DSIZE);
+	
+	/* search the free list for a fit */
 	if ((bp = find_fit(asize)) != NULL){
-//mm_checkheap(12);
 		place(bp, asize);
-//mm_checkheap(13);
 		return bp;
 	}
+	
 	/* not fit found, get more memory and place the block */
 	extendsize = MAX(asize, CHUNKSIZE);
 	if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
 		return NULL;
-//printf("start place\n");
 	place(bp, asize);
-//mm_checkheap(12);
-//printf("finish place: %p\n", bp);
-//mm_checkheap(0);	
-//printf("hi:%p\n",mem_heap_hi());
-return bp;
+	return bp;
 }
 
 /*
  * extend heap
+ * call mem_sbrk to get a free block
+ * put the block into free list
  */
 void *extend_heap(size_t words){
-    void *bp;
-    size_t size;
-//printf("extend\n");	
-    /* allocate an even number of words to maintain alignment */
-    size = words*WSIZE;
-    if((bp = mem_sbrk(size)) == (void *)-1) return NULL;
+	void *bp;
+	size_t size;
 
-    
+	size = words*WSIZE;
+	if((bp = mem_sbrk(size)) == (void *)-1) return NULL;
+	
 	/* initialize free block hdr/ftr */
-    PUT(HDRP(bp), PACK(size, 0)); //Free block hdr
-    PUT(FTRP(bp), PACK(size, 0)); //Free block ftr
-    SET_NEXT(bp, GET_NEXT(free_listp));
+	PUT(HDRP(bp), PACK(size, 0)); //Free block hdr
+	PUT(FTRP(bp), PACK(size, 0)); //Free block ftr
+	
+	/* put it in the free list */
+	SET_NEXT(bp, GET_NEXT(free_listp));
 	SET_PREV(bp, free_listp);
 	SET_NEXT(free_listp, bp);
 	SET_PREV(GET_NEXT(bp), bp);
-//mm_checkheap(3);	
+	
 	/* re-init epilogue hdr */
 	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
-//mm_checkheap(3);
-    /* coalesce if the previous block was free */
-    return coalesce(bp);
+	
+	/* coalesce free blocks */
+	return coalesce(bp);
 }
 
 /*
- * find fit in explicit list
+ * find fit in explicit free list
+ * traverse the list, if free block size > required size, fit!
+ * reach the end of the list, then no fit, return NULL.
  */
 void *find_fit(size_t size){
 	void *bp = GET_NEXT(free_listp);
@@ -184,42 +217,47 @@ void *find_fit(size_t size){
 
 /*
  * coalesce
+ * coalesce consecutive free blocks
+ * use hdr and ftr to do constant time coalesce
  */
 void *coalesce(void *bp){
-    size_t prev_alloc = GET(FTRP(PREV_BLKP(bp))) & 0x1;
-    size_t next_alloc = GET_ALLOC(NEXT_BLKP(bp));
-//printf("%p %p %p %d\n", bp, PREV_BLKP(bp),NEXT_BLKP(bp), (int)(prev_alloc+next_alloc));
-    if(prev_alloc && next_alloc){
-        /* prev/next blocks are not free */
-    }else if(prev_alloc && !next_alloc){
+	size_t prev_alloc = GET(FTRP(PREV_BLKP(bp))) & 0x1;
+	size_t next_alloc = GET_ALLOC(NEXT_BLKP(bp));
+	if(prev_alloc && next_alloc){
+		/* prev/next blocks are not free */
+	}else if(prev_alloc && !next_alloc){
 		/* next block is free */
-        coalesce_next(bp);
-    }else if(!prev_alloc && next_alloc){
+		coalesce_next(bp);
+	}else if(!prev_alloc && next_alloc){
 		/* prev block is free */
 		bp = PREV_BLKP(bp);
-//printf("@@@@@@@@@@@@@@%p\n", bp);
 		coalesce_next(bp);
-    }else{
-        coalesce_next(bp);
+	}else{
+		coalesce_next(bp);
 		bp = PREV_BLKP(bp);
 		coalesce_next(bp);
-    }
-//printf("coal finish\n");
-//mm_checkheap(20);
-    return bp;
+	}
+	return bp;
 }
 
 /*
  * coalesce next block with current block
+ * this method only called by coalesce,
+ * pre-condistion is bp block is free and its next block in heap is free too,
+ * so we merge them
  */
 void coalesce_next(void *bp){
-    size_t size = GET_SIZE(bp);
-    void *next_bp = NEXT_BLKP(bp);	
-    SET_NEXT(GET_PREV(next_bp), GET_NEXT(next_bp));
-    SET_PREV(GET_NEXT(next_bp), GET_PREV(next_bp));
-    size += GET_SIZE(NEXT_BLKP(bp));
-    PUT(HDRP(bp), PACK(size, 0));
-    PUT(FTRP(bp), PACK(size, 0));
+	size_t size = GET_SIZE(bp);
+	void *next_bp = NEXT_BLKP(bp);	
+	
+	/* delete next_bp from free list */
+	SET_NEXT(GET_PREV(next_bp), GET_NEXT(next_bp));
+	SET_PREV(GET_NEXT(next_bp), GET_PREV(next_bp));
+	
+	/* reset hdr/ftr of the new free block */
+	size += GET_SIZE(NEXT_BLKP(bp));
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
 }
 
 /*
@@ -227,94 +265,106 @@ void coalesce_next(void *bp){
  */
 void *place(void *bp, size_t size){
 	void *free_bp;
-	size_t full_size = GET_SIZE(bp);
-//mm_checkheap(44444);
+	size_t full_size = GET_SIZE(bp); //size of the free block before place
+	
+	/* not split the free block for small allocation or small space left */
 	if ((full_size - size) < FSIZE || size < FSIZE ){
-		/* not split the free block for small allocation or small space left */
+		/* reset hdr/ftr the alloc bit */
 		PUT(HDRP(bp), PACK(full_size, 1));
 		PUT(FTRP(bp), PACK(full_size, 1));
+		
+		/* delete from free list */
 		SET_NEXT(GET_PREV(bp), GET_NEXT(bp));
 		SET_PREV(GET_NEXT(bp), GET_PREV(bp));
 		return bp;
 	}
-	/* split the free block, build the new free block */
-	free_bp = bp + size;
-	PUT(HDRP(free_bp), PACK(full_size-size, 0));
+	
+	/* after alloc size bytes, 
+	 * the original free block still have bytes in the tail,
+	 * split the free block into two parts: |alloc one|free one| */
+	free_bp = bp + size; 
+	/* reset ftr/hdr of the new free block*/
+	PUT(HDRP(free_bp), PACK(full_size-size, 0)); 
 	PUT(FTRP(free_bp), PACK(full_size-size, 0));
+	/* add it to free list, also delete the original free block from list */
 	SET_NEXT(free_bp, GET_NEXT(bp));
 	SET_PREV(free_bp, GET_PREV(bp));
 	SET_NEXT(GET_PREV(bp), free_bp);
 	SET_PREV(GET_NEXT(bp), free_bp);
+	/* set hdr/ftr of alloc block */
 	PUT(HDRP(bp), PACK(size, 1));
 	PUT(FTRP(bp), PACK(size, 1));
 	return bp;
 }
 
-
 /*
  * free
+ * free a block and add it the the free list, then coalesce
  */
 void free (void *ptr) {
 	size_t size;
-//printf("free,%p\n",ptr);
-if (ptr == NULL)return;
-size = GET_SIZE(ptr);
-//printf("free,%p,%d\n",ptr,(int)GET_SIZE(ptr));
-//mm_checkheap(33333);
-    PUT(HDRP(ptr), PACK(size, 0));
-    PUT(FTRP(ptr), PACK(size, 0));
-    SET_NEXT(ptr, GET_NEXT(free_listp));
-    SET_PREV(ptr, free_listp);
-    SET_PREV(GET_NEXT(ptr), ptr);
-    SET_NEXT(free_listp, ptr);    
-    coalesce(ptr);
+	if (ptr == NULL)return;
+	size = GET_SIZE(ptr);
+	/* reset hdr/ftr of alloc bit */
+	PUT(HDRP(ptr), PACK(size, 0));
+	PUT(FTRP(ptr), PACK(size, 0));
+	/* add to free list */
+	SET_NEXT(ptr, GET_NEXT(free_listp));
+	SET_PREV(ptr, free_listp);
+	SET_PREV(GET_NEXT(ptr), ptr);
+	SET_NEXT(free_listp, ptr);    
+	
+	coalesce(ptr);
 }
 
 /*
- * realloc - you may want to look at mm-naive.c
+ * realloc
+ * if realloc size is larger than old one: malloc new space and copy
+ * if realloc size is smaller: shrink the block, 
+ * and release the space as a free block
  */
 void *realloc(void *oldptr, size_t size) {
 	size_t asize;
-    size_t oldsize;
-    void *newptr;
+	size_t oldsize;
+	void *newptr;
 	void *free_bp;
-//printf("re\n");
-//mm_checkheap(1122);
-    /* If size == 0 then this is just free, and we return NULL. */
-    if(size == 0) {
+
+	/* If size == 0 then this is just free, and we return NULL. */
+	if(size == 0) {
 		free(oldptr);
 		return 0;
-    }
+	}
 	
 	/* If oldptr is NULL, then this is just malloc. */
-    if(oldptr == NULL) {
+	if(oldptr == NULL) {
 		return malloc(size);
-    }
+	}
 	
 	asize = ALIGN(size + 2*WSIZE);
 	oldsize = GET_SIZE(oldptr);
 	if (oldsize < asize){
-		
+		/* if realloc size is larger than old one: malloc new space and copy*/
 		newptr = malloc(size);
 		memcpy(newptr, oldptr, oldsize - 2*WSIZE);
-        free(oldptr);
-        return newptr;
+		free(oldptr);
+		return newptr;
 		
 	}else if (oldsize > asize){
 		/* realloc a smaller space */
 		if ((oldsize - asize) < FSIZE || asize < FSIZE ){
-			/* small space, no changes */
+			/* no changes */
 			return oldptr;
 		}else{
-//mm_checkheap(111);
 			free_bp = oldptr + asize;
+			/* set alloc bit to 0*/
 			PUT(HDRP(free_bp), PACK(oldsize-asize, 0));
 			PUT(FTRP(free_bp), PACK(oldsize-asize, 0));
+			/* add to free list */
 			SET_NEXT(free_bp, GET_NEXT(free_listp));
 			SET_PREV(free_bp, free_listp);
 			SET_NEXT(free_listp, free_bp);
 			SET_PREV(GET_NEXT(free_bp), free_bp);
-			
+			/* change the alloced block size to new size*/ 
 			PUT(HDRP(oldptr), PACK(asize, 1));
 			PUT(FTRP(oldptr), PACK(asize, 1));
 			return oldptr;
@@ -329,14 +379,13 @@ void *realloc(void *oldptr, size_t size) {
  * calloc - you may want to look at mm-naive.c
  * This function is not tested by mdriver, but it is
  * needed to run the traces.
+ * same as that in naive file.
  */
 void *calloc (size_t nmemb, size_t size) {
 	size_t bytes = nmemb * size;
 	void *newptr;
-//printf("call\n");
 	newptr = malloc(bytes);
 	memset(newptr, 0, bytes);
-
 	return newptr;
 }
 
@@ -346,7 +395,7 @@ void *calloc (size_t nmemb, size_t size) {
  * May be useful for debugging.
  */
 static int in_heap(const void *p) {
-    return p <= mem_heap_hi() && p >= mem_heap_lo();
+	return p <= mem_heap_hi() && p >= mem_heap_lo();
 }
 
 /*
@@ -354,7 +403,7 @@ static int in_heap(const void *p) {
  * May be useful for debugging.
  */
 static int aligned(const void *p) {
-    return (size_t)ALIGN(p) == (size_t)p;
+	return (size_t)ALIGN(p) == (size_t)p;
 }
 
 /*
@@ -364,35 +413,32 @@ void mm_checkheap(int lineno) {
 	
 	void *bp = heap_listp;
 	int last_free = 0;
-	int count = 0;
-//	printf("AT line %d we run test!\n", lineno);
+	int count = 0; //how many free blocks
+	
 	/* check prologue */
 	if (GET(bp) != PACK(DSIZE, 1)){
-		printf("wrong prologue!%d\n",lineno);
+		printf("wrong prologue, at line: %d\n",lineno);
 	}
 	
 	while (1){
 		if (GET_SIZE(bp) == 0 && GET_ALLOC(bp) == 1){
 			/* reach epilogue */
-			//printf("epilogue %p\n", bp);
-			//printf("last byte %p\n", mem_heap_hi());
 			if (bp-1 != mem_heap_hi()){
 				printf("wrong epilogue,%d\n",lineno);
 			}
 			break;
 		}else{
-//printf("hdr:%p,ftr:%p,value:%p\n",HDRP(bp),FTRP(bp),(void *)GET(HDRP(bp)));
 			/* check aligned */
 			if (!aligned(bp)){
-				printf("not aligned, bp = %d!\n", lineno);
+				printf("not aligned, at line: %d!\n", lineno);
 			}
 			/* check ftr == hdr */
 			if (GET(HDRP(bp)) != GET(FTRP(bp))){
-				printf("hdr != ftr, bp = %d!\n", lineno);
+				printf("hdr != ftr, at line: %d!\n", lineno);
 			}
 			/* check no consecutive free block */
 			if (last_free && GET_ALLOC(bp) == 0){
-				printf("consecutive free blocks, bp = %d!\n", lineno);
+				printf("consecutive free blocks, at line: %d!\n", lineno);
 			}
 			last_free = !GET_ALLOC(bp);
 			count += last_free;
@@ -410,22 +456,20 @@ void mm_checkheap(int lineno) {
 		}else{
 			/* check free */
 			if (GET_ALLOC(bp)){
-				printf("allocated block in free list! line = %d!\n", lineno);
+				printf("allocated block in free list! at line: %d!\n", lineno);
 			}
 			/* check cur.next.prev == cur and cur.prev.next == cur */
 			if (GET_PREV(GET_NEXT(bp)) != bp || GET_NEXT(GET_PREV(bp)) != bp){
-				printf("inconsistent pointers! bp = %d!\n", lineno);
+				printf("inconsistent pointers! at line: %d!\n", lineno);
 			}
 			if (!in_heap(bp)){
-				printf("not in heap!%d\n", lineno);
+				printf("not in heap! at line: %d\n", lineno);
 			}
 			count--;
 		}
 	}
 	/* check free block numbers are the same through two traversing methods */
 	if (count){
-		printf("inconsistent free block numbers in two checks!\n");
+		printf("inconsistent free block numbers in two checks!, at line: %d\n", lineno);
 	}
-//	printf("AT line %d the test finished!\n", lineno);
-	
 }
---
+++ hw7/proxylab-handout/proxy.c
@@ -0,0 +1,312 @@
+#include <stdio.h>
+
+/* Recommended max cache and object sizes */
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+#define MAX_LINE 1024
+struct connfd_timed{
+    int connfd; // client socket
+    int timestamp; // the timestamp of this connection, used for LRU caching
+};
+
+struct cache_block{
+    char url[MAX_LINE]; // cached url
+    char response[MAX_OBJECT_SIZE]; // the cached http response
+	int timestamp; // last operation timestamp
+	int readcnt;
+	int write_request;
+	sem_t mutex, w;
+};
+
+/* You won't lose style points for including these long lines in your code */
+static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *accept_hdr = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
+static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
+static const char *connection_hdr = "Connection: close\r\n";
+static const char *proxy_connection_hdr = "Proxy-Connection: close\r\n";
+
+static int cache_cnt;
+static int time_cur;
+
+int main(int argc, char **argv)
+{
+	int port, listenfd, clientlen, connfd;
+	struct socketaddr_in clientaddr;
+	struct connfd_timed *clientconnfd;
+	pthread_t tid;
+    printf("%s%s%s", user_agent_hdr, accept_hdr, accept_encoding_hdr);
+	
+	/* init cache blocks */
+	cache_cnt = MAX_CACHE_SIZE / MAX_OBJECT_SIZE;
+	cache_init();
+	time_cur =0;
+	
+	/* check port */
+    if (argc != 2){
+        exit(0);
+    }
+	port = atoi(argv[1]);
+	
+	/* listen connection */
+    listenfd = Open_listenfd(port);
+    
+	/* infinite loop for client connection */
+    while(1) {
+		/* accept connection */
+		clientconnfd = (struct connfd_timed *)Malloc(sizeof(struct connfd_timed));
+		clientlen = sizeof(clientaddr);
+        clientconnfd->connfd = Accept(listenfd, (SA*)(&clientaddr), &clientlen);
+		clientconnfd->timestamp = time_cur++;
+		/* create new thread to handle the connection */
+        Pthread_create(&tid, NULL, thread, (void *)(&clientconnfd));
+    }
+    return 0;
+}
+
+void *thread(void *vargp){
+	struct connfd_timed *clientconnfd = (struct connfd_timed *)vargp;
+	int connfd = clientconnfd->connfd;
+	int timestamp = clientconnfd->timestamp;
+	rio_t rio;
+	char buf[MAX_LINE], method[MAX_LINE], version[MAX_LINE], url[MAX_LINE];
+	char host[MAX_LINE], port[MAX_LINE], query[MAX_LINE];
+	int i;
+	char *response;
+	int clientfd;
+	int count;
+	int content_length;
+	
+	
+	/* read socket */
+    rio_readinitb(&rio, connfd);
+    rio_readlineb(&rio, buf, MAX_LINE);
+    sscanf(buf, "%s %s %s", method, url, http);
+
+	/* only GET */
+    if(strcasecmp(method, "GET")){
+        Close(connfd);
+        return NULL;
+    }
+	
+    /* cache hit */
+	response=(char*)Malloc(MAX_OBJECT_SIZE);
+	response[0]='\0';
+	if (cache_read(url, response)){
+		rio_writen(connfd, response, strlen(response));
+		Close(connfd);
+		free(response);
+		return NULL;
+	}
+	
+	/* cache miss, create http request to real server*/
+	
+	/* illegal url */
+    if (parse_url(url, host, port, query)){
+		Close(connfd);
+		return NULL;
+	}
+	
+	/* connect server failed */
+    if((clientfd = connect_server(host, port, query)) < 0){
+        Close(connfd);
+        return NULL;
+    }
+
+	/* read http response head, store content length */
+	response = (char*)Malloc(MAX_OBJECT_SIZE);
+    rio_readinitb(&rio, clientfd);
+    content_length = 0;
+	while ((count = rio_readlineb(&rio, buf, MAX_LINE)) > 0){
+		rio_writen(connfd, buf, count);
+		strncat(response, buf, count);
+		if(strstr(buf, "Content-length")){
+            sscanf(buf, "Content-length: %d\r\n", &content_length);
+		}
+		if (strcmp(buf,"\r\n") == 0){
+			break;
+		}
+	}
+	
+	/* too big to cache */
+    if(content_length + strlen(response) >= MAX_OBJECT_SIZE){
+		while ((count = rio_readnb(&rio, buf, MAX_LINE)) > 0){
+			rio_writen(connfd, buf, count);
+		}
+		Close(clientfd);
+		Close(connfd);
+		free(response);
+		return NULL;
+	}
+	
+	/* add to cache */
+	while ((count = rio_readnb(&rio, buf, MAX_LINE)) > 0){
+		strncat(response, buf, count);
+		rio_writen(connfd, buf, count);
+	}
+	cache_add(response, url, timestamp);
+	Close(clientfd);
+    Close(connfd);
+    free(response);
+    return NULL;
+}
+
+/* 
+ * parse url
+ * return 0 if succeed, else failed
+ */
+int parse_url(char *url, char *hostname, char *port, char *query){
+    char *temp;
+	int i = 0;
+	int j = 0;
+	char c;
+	char *pointer;
+	int in_query = 0;
+    
+	/* skip http:// */
+	temp = strstr(url, "//");
+    if(temp != NULL) {
+        temp = temp + 2;
+    } else {
+        temp = url;
+    }
+	
+	/* parse */
+	pointer = hostname;
+	while ((c = (char *)(temp + i)) != '\0'){
+		if (c == ':'){
+			if (in_query){
+				return 1;
+			}
+			pointer = port;
+			j = 0;
+		}else if (c == '/' && !in_query){
+			in_query = 1;
+			pointer = query;
+			j = 0;
+		}else{
+			pointer[j] = temp[i];
+		}
+		i++;
+		j++;
+	}
+	if (port[0] == '\0'){
+		port[0] = '8';
+		port[1] = '0';
+	}
+	return 0;
+}
+
+/* 
+ * proxy as a client to connect server,
+ * return clientfd, if clientfd < 0, means failed 
+ */
+int connect_server(char *hostname, char *port, char *query){
+    char buf[MAXLINE];
+    int clientfd = open_clientfd(hostname, atoi(port));
+
+    /* connection failed */
+    if(clientfd < 0)
+        return clientfd;
+
+    /* send http request */
+    sprintf(buf, "GET %s HTTP/1.0\r\n", query);
+    Rio_writen(clientfd, buf, strlen(buf));
+    sprintf(buf, "Host: %s\r\n", hostname);
+    Rio_writen(clientfd, buf, strlen(buf));
+    Rio_writen(clientfd, user_agent_hdr, strlen(user_agent_hdr));
+    Rio_writen(clientfd, accept_hdr, strlen(accept_hdr));
+	Rio_writen(clientfd, accept_encoding_hdr, strlen(accept_encoding_hdr));
+    Rio_writen(clientfd, connection_hdr, strlen(connection_hdr));
+	Rio_writen(clientfd, proxy_connection_hdr, strlen(proxy_connection_hdr));
+    Rio_writen(clientfd, "\r\n", strlen("\r\n"));
+    return clientfd;
+}
+
+void cache_init(){
+	for (i=0;i<cache_cnt;i++){
+		caches[i].timestamp = 0;    
+		caches[i].url[0] = '\0';
+		caches[i].response[0] = '\0';
+		caches[i].readcnt = 0;
+		Sem_init(&caches[i].mutex, 0, 1);
+		Sem_init(&caches[i].w, 0, 1);
+	}
+}
+
+/*
+ * find a hit in cache
+ * if miss, return 0,
+ * if hit, fill response, and return 1
+ */
+int cache_read(char *url, char *response){
+	int i;
+	for (i = 0; i<cache_cnt; i++){
+		while (caches[i].write_request){
+			/* not add new read, let write first */
+		}
+		
+		/* lock to add readcnt*/
+		P(&(caches[i].mutex));
+		caches[i].readcnt++;
+		if (caches[i].readcnt == 1){
+			P(&(caches[i].w));
+		}
+		V(&(caches[i].mutex));
+		
+		/* hit */
+		if (strcmp(url, caches[i].url) == 0){
+			/* copy http response */
+			strcpy(response, caches[i].response);
+			/* update last timestamp, and readcnt--, return */
+			P(&(caches[i].mutex));
+			caches[i].readcnt--;
+			if (timestamp>caches[i].timestamp){
+				caches[i].timestamp= timestamp;
+			}
+			if (caches[i].readcnt == 0){
+				V(&(caches[i].w));
+			}
+			V(&(caches[i].mutex));
+			return 1;
+		}
+		
+		/* lock to readcnt-- */
+		P(&(caches[i].mutex));
+		caches[i].readcnt--;
+		if (caches[i].readcnt == 0){
+			V(&(caches[i].w));
+		}
+		V(&(caches[i].mutex));
+	}
+	return 0;
+	
+}
+
+/*
+ * add new http response to cache
+ * using LRU evict policy
+ */
+void cache_write(char *response, char *url, int timestamp){
+	int i;
+	int evict;
+	int min_timestamp = 0x7fffffff;
+	
+	/* find the victim, who has smallest timestamp 
+	 * not strict LRU, because we didn't lock, timestamp may be changed.
+	 */
+	for (i=0;i<cache_cnt;i++){
+		if (caches[i].timestamp < min_timestamp){
+			min_timestamp = caches[i].timestamp
+			evict = i;
+		}
+	}
+	caches[evict].write_request = 1;
+	
+	/* write in new http response */
+	P(&(caches[evict].w));
+	strcpy(caches[evict].response, response);
+	strcpy(caches[evict].url, url);
+	caches[evict].timestamp = timestamp;
+	caches[evict].write_request = 0;
+	V(&(caches[evict].w));
+}
